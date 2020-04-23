7caf6975c4346db51a1720fad95173137f81ecc4
Ushankar-1 <53460303+Ushankar-1@users.noreply.github.com>
2020-04-22 23:35:33 -0700

    Added Poem Generator
    Added unfinished Poem Generator program.

---
+++ Poem_Generator.py
@@ -0,0 +1,48 @@
+import time
+import random
+
+print('Welcome to your poem generator!\n')
+time.sleep(2)
+
+print('Choose three words from the following list!')
+time.sleep(3)
+
+print('''
+1. Love
+2. Pain
+3. Star
+4. Trust
+5. Hope
+6. Fall
+7. War
+8. Sun
+9. Begin
+10. Dream
+11. Tears
+12. Dark
+13. Flame
+14. Truth
+15. End
+''')
+
+time.sleep(3)
+
+firstword = int(input('What is the number of your first word? '))
+print('\nExcellent choice!')
+time.sleep(1.5)
+
+secondword = int(input('What is the number of your second word? '))
+print('\nPerfect!')
+time.sleep(1.5)
+
+thirdword = int(input('What is the number of your third word? '))
+
+print(firstword,secondword,thirdword)
+
+#1. Love is not a kind creature
+# Love is proud and vulnerable
+# This love only grows
+# Love dies in your cages
+# Love not lightly
+#2. Pain is just a feeling
+# 
