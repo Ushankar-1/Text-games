eb698a22b8c2eadbdf83ca2d7b187506ecf225ba
Ushankar-1 <53460303+Ushankar-1@users.noreply.github.com>
2020-04-26 21:36:06 -0700

    Added 3 year old simulator
    Added 3 year old simulator to text games

---
+++ 3_yr_old_sim_11:15:16.py
@@ -0,0 +1,30 @@
+import time
+import random
+
+print('Welcome to your Three-year-old simulator.')
+time.sleep(2)
+print('Try and stop the madness.')
+time.sleep(3)
+response = input('\nHi.\n')
+y = 0
+while response != 'because' and response != 'Because' and response != 'because.' and response != 'Because.'and response != 'Because!'and response != 'because!'and response != 'BECAUSE'and response != 'BECAUSE.'and response != 'BECAUSE!':
+    x = random.randint(1,7)
+    if x == 1:
+        response = input('WHY?\n')
+    elif x == 2:
+        response = input('WHAT?\n')
+    elif x == 3:
+        response = input('NO. NONONONONONO.\n')
+    elif x == 4:
+        response = input('GIMMEGIMMEGIMMEITSMINEGIMME\n')
+    elif x == 5:
+        response = input('I WANT MY MOMMY YOU\'RE MEAN!!!\n')
+    elif x == 6:
+        response = input('GOAWAYGOAWAYGOAWAYGOAWAY\n')
+    elif x == 7:
+        response = input('*SCREEEEEEEAAAAAAAAAAAAAMMMMMMMMMMM*\n')
+print('Oh. Okay.')
+        
+
+    
+      
